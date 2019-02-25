5f2bba3fda0bb85ad983252039443edc818cfe01
Felipe Del Canto <felipe.delcanto@icloud.com>
2019-02-25 00:41:41 -0300

    Showing pets on PetView. Pass data from PetView to EditPetView. Back buttons working

--- VetApp/PetController.swift
+++ VetApp/PetController.swift
@@ -15,11 +15,15 @@ class PetViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     let PET_CELL_XIB = "PetCell" // the name of the XIB file
     let ADDPET_CELL_ID = "addPetCell" // the identifier set in the XIB file
     let ADDPET_CELL_XIB = "AddPetCell" // the name of the XIB file
-    let ADDPET_SEGUE_ID = "goToAddPet"
     let PET_DATABASE_ID = "Pets"
     let ESTIMATED_ROW_HEIGHT : CGFloat = 120.0
     var petArray : [Pet] = [Pet]()
 
+    // Segue Consts and Variables
+    let ADDPET_SEGUE_ID = "goToAddPet"
+    let EDITPET_SEGUE_ID = "goToEditPet"
+    var petToEdit : Pet? = nil
+    
     // Outlets
     @IBOutlet weak var petTableView: UITableView!
     
@@ -33,6 +37,19 @@ class PetViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         ConfigureTableView()
         
         petArray.append(Pet(name: "Kira", species: .Cat, race : "DLH", dateOfBirth: 25, chipNumber: 1))
+    }
+    
+    fileprivate func ConformToProtocols() {
+        petTableView.delegate = self
+        petTableView.dataSource = self
+    }
+
+    fileprivate func ConfigureTableView() {
+        petTableView.register(UINib(nibName: ADDPET_CELL_XIB, bundle : nil), forCellReuseIdentifier: ADDPET_CELL_ID)
+        petTableView.register(UINib(nibName: PET_CELL_XIB, bundle : nil), forCellReuseIdentifier: PET_CELL_ID)
+        petTableView.rowHeight = ESTIMATED_ROW_HEIGHT
+//        petTableView.rowHeight = UITableView.automaticDimension
+//        petTableView.estimatedRowHeight = ESTIMATED_ROW_HEIGHT
     }
     
     //////////////////////////////////////////////////////////////
@@ -49,7 +66,6 @@ class PetViewController: UIViewController, UITableViewDelegate, UITableViewDataS
             let cell = tableView.dequeueReusableCell(withIdentifier: PET_CELL_ID, for: indexPath) as! PetCell
             let petToShow : Pet = petArray[indexPath.row]
             cell.petName.text = petToShow.name
-            cell.petPicture.image = UIImage(named: "Kira")
             cell.petRace.text = petToShow.race
             cell.petSpecies.text = petToShow.species.rawValue
             return cell
@@ -59,22 +75,25 @@ class PetViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: ADDPET_SEGUE_ID, sender: self)
+        let numberOfPets : Int = petArray.count
+        
+        if indexPath.row < numberOfPets {
+            petToEdit = petArray[indexPath.row]
+            performSegue(withIdentifier: EDITPET_SEGUE_ID, sender: self)
+        } else {
+            performSegue(withIdentifier: ADDPET_SEGUE_ID, sender: self)
+        }
     }
     
-    fileprivate func ConformToProtocols() {
-        petTableView.delegate = self
-        petTableView.dataSource = self
-    }
+    //////////////////////////////////////////////////////////////
     
-    fileprivate func ConfigureTableView() {
-        petTableView.register(UINib(nibName: ADDPET_CELL_XIB, bundle : nil), forCellReuseIdentifier: ADDPET_CELL_ID)
-        petTableView.register(UINib(nibName: PET_CELL_XIB, bundle : nil), forCellReuseIdentifier: PET_CELL_ID)
-        petTableView.rowHeight = UITableView.automaticDimension
-        petTableView.estimatedRowHeight = ESTIMATED_ROW_HEIGHT
+    // MARK: Segue methods
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == EDITPET_SEGUE_ID {
+            let editPetVC = segue.destination as! EditPetViewController
+            editPetVC.petToEdit = petToEdit
+        }
     }
-
-
 
 }
 
